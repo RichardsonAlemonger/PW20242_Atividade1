feb67a2db7c7364c8e528281af92aa06ba18d683
RichardsonAlemonger <clayalemonger@gmail.com>
2024-10-01 21:50:29 -0300

    subir

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import uvicorn
-
-app = FastAPI()
-templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.get("/")
-def read_index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-if __name__ == "__main__":
- uvicorn.run("main:app", port=8000, reload=True)
+from fastapi import FastAPI, Form 
+from fastapi.templating import Jinja2Templates 
+from fastapi.staticfiles import StaticFiles 
+from fastapi.responses import HTMLResponse, RedirectResponse 
+from starlette.requests import Request 
+import uvicorn 
+ 
+app = FastAPI() 
+ 
+templates = Jinja2Templates(directory="templates") 
+ 
+app.mount("/static", StaticFiles(directory="static"), name="static") 
+ 
+@app.get("/", response_class=HTMLResponse) 
+def paginaInicial(request: Request): 
+  return templates.TemplateResponse("index.html", {"request": request}) 
+ 
+@app.get("/cadastro", response_class=HTMLResponse) 
+async def cadastro(request: Request): 
+  return templates.TemplateResponse("cadastro.html", {"request": request}) 
+ 
+@app.post("/post_cadastro") 
+async def post_cadastro( 
+    nome: str = Form(...),  
+    descricao: str = Form(...), 
+    estoque: int = Form(...), 
+    preco: float = Form(...),  
+    categoria: str = Form(...)): 
+  return RedirectResponse("/", status_code=303) 
+ 
+ 
+if __name__ == "__main__": 
+  uvicorn.run("main:app", port=8000, reload=True)
